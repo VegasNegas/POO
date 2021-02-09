608b0356170b5ec3e6a6ddb55ff0d0818c9f6805
Glaucio Oliveira <glauciodeoliveira123@gmail.com>
2021-02-09 20:37:49 -0300

    Exercicio 3

---
+++ Exercicio_Sintaxe_Java/Ex_3/src/main/java/third.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class third {
+    public static void main(String[] args) {
+        int total = 0;
+        for(int i = 1; i < 1000; i++){
+            total += i;
+        }
+        System.out.println(total);
+    }    
+}
