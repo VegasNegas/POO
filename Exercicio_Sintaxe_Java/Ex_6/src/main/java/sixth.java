edcf5fba2c1a377cfaacf4394978b0cd6585753b
Glaucio Oliveira <glauciodeoliveira123@gmail.com>
2021-02-09 22:00:22 -0300

    Exercicio 6

---
+++ Exercicio_Sintaxe_Java/Ex_6/src/main/java/sixth.java
@@ -0,0 +1,15 @@
+import javax.sound.midi.SysexMessage;
+
+public class sixth {
+    public static void main(String[] args) {
+        int temp_1 = 0, temp_2 = 1;
+        do{
+            System.out.print(temp_1 + " ");
+            temp_2 += temp_1;
+            temp_1 += temp_2;
+            System.out.print(temp_2 + " ");
+        
+        }while(temp_1 < 100);
+    
+    }
+}
